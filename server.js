fbc28f100afac52ed7d196afc1a45262355337d6
pulikkoden <anilinside@gmail.com>
2016-09-26 19:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content = {
+  title:'Article one| Anil Varghese',
+  heading:'Article one',
+  date:'September 1 2016',
+  content:'This is my sample content for one'
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
