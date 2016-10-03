6e1dfcab3e7ad40e5f6739a82cd75ae107b4b20c
pulikkoden <anilinside@gmail.com>
2016-10-03 19:24:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter= 0;
+button.onclick = function(){
+    
+    //send request for counter to server
+    
+    //capture the response
+    
+    //render the resonse to span element
+    
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
